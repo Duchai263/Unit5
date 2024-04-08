8a9c8854da687aed77e60de56ffcdef6b559278a
Duchai263 <95674277+Duchai263@users.noreply.github.com>
2024-04-08 19:49:44 +0700

    Create Database

--- App.py
+++ App.py
@@ -1,11 +1,25 @@
-from flask import Flask,render_template
-
-app = Flask(__name__)
+from flask import Flask,render_template,request
+from flask_sqlalchemy import SQLAlchemy
+from model import db, app, Project
 
 @app.route('/')
-def about():
+def index():
     return render_template("index.html") 
 
-@app.route('/projectform')
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+
+@app.route('/project/new', methods = ['GET','POST'])
 def pronjectfrom():
-    return render_template("detail.html")       
+    if request.form:
+        print(request.form)
+    return render_template("projectform.html")
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Date, ARRAY, Text
+from datetime import datetime
+
+app = Flask(__name__,static_folder="static")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///projects.db"
+db = SQLAlchemy(app)
+
+class Project(db.Model):
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    date = Column(Date,default=datetime.now().date())
+    description = Column(Text)
+    skill = Column(String)
+
+    def __repr__(self):
+        return f"""Project: (Title: {self.title}
+                            Date: {self.date}
+                            Description: {self.description}
+                            Skill: {self.skill})"""
