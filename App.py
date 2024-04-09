a4523c4ebbdef76f77dbd2fe812ebae003e7d142
Duchai263 <95674277+Duchai263@users.noreply.github.com>
2024-04-09 15:26:03 +0700

    Update App.py

--- App.py
+++ App.py
@@ -12,19 +12,20 @@ def index():
 def about():
     return render_template("about.html")
 
-@app.route('/detail')
-def detail():
+@app.route('/project/<id>')
+def detail(id):
     return render_template("detail.html")
-
 
 
 @app.route('/project/new', methods = ['GET','POST'])
 def projectformm():
     if request.form:
         data = request.form
-        print(datetime.date)
-        print(data['date'])
-        project = Project(title = data['title'], date = datetime.strptime(data['date'],date_format), skills = data['skills'], description = data['desc'], github = data['github'] )
+        project = Project(title = data['title'], 
+                          date = datetime.strptime(data['date'],date_format), 
+                          skills = data['skills'], 
+                          description = data['desc'], 
+                          github = data['github'] )
         db.session.add(project)
         db.session.commit()
     return render_template("projectform.html")
